78de39fe069843cf0736246822c3d425e6ddc74d
Alexey Belkov <lexabelco2013@yandex.ru>
2022-11-09 02:44:07 +0500

    feat: сверстать header

--- src/App.js
+++ src/App.js
@@ -2,8 +2,37 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="page">
+      <header className='header'>
+        <div className='header__logo'>
+          <img className='header__logo-img' src='./images/logo.svg' alt='Логотип'></img>
+          <div className='header__logo-info'>
+            <h3 className='header__logo-title'>REACT SNEAKERS</h3>
+            <p className='header__logo-subtitle'>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+
+        <nav>
+          <ul className='header__menu'>
+            <li>
+              <button className='header__menu-button'>
+                <img className='header__menu-icon' src='./images/menu-basket-icon.svg'></img>
+                <span className='header__menu-text'>1205 руб.</span>
+              </button>
+            </li>
+            <li>
+              <button className='header__menu-button'>
+                <img className='header__menu-icon' src='./images/menu-heart-icon.svg'></img>
+              </button>
+            </li>
+            <li>
+              <button className='header__menu-button'>
+                <img className='header__menu-icon' src='./images/menu-profile-icon.svg'></img>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
