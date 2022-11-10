646caf6a871d9b7f880cd24399f0c46fc82b828b
Alexey Belkov <lexabelco2013@yandex.ru>
2022-11-10 23:56:42 +0500

    feat: реализовать подгрузку карточек с бэка

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Card from './components/Card/Card';
-
-const cardsItems = [
-  {
-    linkImg: './images/card-item-1.jpg',
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 12999
-  },
-  {
-    linkImg: './images/card-item-2.jpg',
-    title: 'Мужские Кроссовки Nike Air Max 270',
-    price: 10999
-  },
-  {
-    linkImg: './images/card-item-3.jpg',
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 11999
-  },
-  {
-    linkImg: './images/card-item-4.jpg',
-    title: 'Кроссовки Puma X Aka Boku Future Rider',
-    price: 8999
-  }
-]
+import React from 'react';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://636d0228ab4814f2b275a28e.mockapi.io/react-sneakers')
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      setItems(json);
+    });
+  }, []);
+
+
   return (
     <div className="page">
+
+      <div className='basket'>
+        <div className='basket__container'>
+          <div className='basket__header'>
+            <h2 className='basket__title'>Корзина</h2>
+            <button className='basket__close-btn'></button>
+          </div>
+        </div>
+      </div>
+
       <Header />
       <div className='cards'>
         <div className='cards__header'>
@@ -38,13 +39,15 @@ function App() {
           </div>
         </div>
         <ul className='cards__list'>
-          {cardsItems.map((item) => (
+          {
+            items.map((item, index) =>
             <Card
+              key={index}
               linkImg={item.linkImg}
               title={item.title}
               price={item.price}
-            />
-          ))}
+            />)
+          }
         </ul>
       </div>
     </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './card.scss';
 
 const Card = (props) => {
+
+  const [isAdded, addCard] = useState(false);
+  const [isLiked, likeCard] = useState(false);
+
   return (
     <li>
       <article className='card'>
         <div className='card__img-container'>
-          <button className='card__like-btn'></button>
-          <img className='card__img' src={props.linkImg}></img>
+          <button className={'card__like-btn ' + (isLiked ? 'card__like-btn_active' : '')} onClick={ () => {likeCard(!isLiked);} }></button>
+          <img className='card__img' src={props.linkImg} alt="Фоторграфия кроссовок"></img>
         </div>
         <h2 className='card__title'>{props.title}</h2>
         <div className='card__footer'>
@@ -15,7 +19,7 @@ const Card = (props) => {
             <p className='card__price-title'>Цена:</p>
             <span className='card__price'>{props.price} руб.</span>
           </div>
-          <button className='card__add-btn'></button>
+          <button className={'card__add-btn ' + (isAdded ? 'card__add-btn_active' : '')} onClick={ () => {addCard(!isAdded);} }></button>
         </div>
       </article>
     </li>
