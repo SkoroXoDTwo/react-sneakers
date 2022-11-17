adbd687fb6691467d89d7adeaa5c5fdda0e26c4a
Alexey Belkov <lexabelco2013@yandex.ru>
2022-11-18 01:06:52 +0500

    feat: сверстать уведомление пустой корзины

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -1,49 +1,59 @@
 import React from 'react';
 import './basket.scss';
 
-const Basket = ({items = [], onClickCloseBtn, onRemoveItem}) => {
+const Basket = ({ items = [], onClickCloseBtn, onRemoveItem }) => {
 
 
   return (
     <div className={'basket'}>
-        <div className='basket__container'>
-          <div className='basket__header'>
-            <h2 className='basket__title'>Корзина</h2>
-            <button className='basket__close-btn' onClick={onClickCloseBtn}></button>
-          </div>
+      <div className='basket__container'>
+        <div className='basket__header'>
+          <h2 className='basket__title'>Корзина</h2>
+          <button className='basket__close-btn' onClick={onClickCloseBtn}></button>
+        </div>
 
-          <ul className='basket__list'>
-
-            {items.map((item, index) =>
-              <li key={index}>
-                <article className='basket__item'>
-                  <img className='basket__item-img' src={`${item.linkImg}`}></img>
-                  <div className='basket__item-info'>
-                    <h3 className='basket__item-title'>{item.title}</h3>
-                    <p className='basket__item-price'>{item.price} руб.</p>
-                  </div>
-                  <div className='basket__item-delete-btn' onClick={() => onRemoveItem(item.id)}></div>
-                </article>
-              </li>
-              )}
-
-          </ul>
-          <div className='basket__count-container'>
-            <p className='basket__count-title'>Итого:</p>
-            <span className='basket__count-line'></span>
-            <span className='basket__count-price'>21 498 руб.</span>
-          </div>
-          <div className='basket__count-container'>
-            <p className='basket__count-title'>Доствка 5%</p>
-            <span className='basket__count-line'></span>
-            <span className='basket__count-price'>1074 руб.</span>
-          </div>
-          <button className='basket__process-btn'>
-            <p className='basket__btn-text'>Оформить заказ</p>
-            <span className='basket__btn-arrow'></span>
+        <div className='basket__notice'>
+          <img className='basket__notice-img' src="./images/basket-notice-null.svg"/>
+          <h3 className='basket__notice-title'>Корзина пустая</h3>
+          <p className='basket__notice-text'>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
+          <button className='basket__notice-btn'>
+            <span className='basket__btn-arrow basket__btn-arrow_notice'></span>
+            <p className='basket__btn-text'>Вернуться назад</p>
           </button>
         </div>
+
+        <ul className='basket__list'>
+
+          {items.map((item, index) =>
+            <li key={index}>
+              <article className='basket__item'>
+                <img className='basket__item-img' src={`${item.linkImg}`}></img>
+                <div className='basket__item-info'>
+                  <h3 className='basket__item-title'>{item.title}</h3>
+                  <p className='basket__item-price'>{item.price} руб.</p>
+                </div>
+                <div className='basket__item-delete-btn' onClick={() => onRemoveItem(item.id)}></div>
+              </article>
+            </li>
+          )}
+
+        </ul>
+        <div className='basket__count-container'>
+          <p className='basket__count-title'>Итого:</p>
+          <span className='basket__count-line'></span>
+          <span className='basket__count-price'>21 498 руб.</span>
+        </div>
+        <div className='basket__count-container'>
+          <p className='basket__count-title'>Доствка 5%</p>
+          <span className='basket__count-line'></span>
+          <span className='basket__count-price'>1074 руб.</span>
+        </div>
+        <button className='basket__process-btn'>
+          <p className='basket__btn-text'>Оформить заказ</p>
+          <span className='basket__btn-arrow'></span>
+        </button>
       </div>
+    </div>
   );
 };
 
