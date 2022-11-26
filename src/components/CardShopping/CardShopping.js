7bd3bb11b8008c05e7c698b1db355daf388a1bef
Alexey Belkov <lexabelco2013@yandex.ru>
2022-11-27 01:32:17 +0500

    feat: добавить дату, сумму, изображения карточкам заказов

--- src/components/CardShopping/CardShopping.js
+++ src/components/CardShopping/CardShopping.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './cardShopping.scss';
 import { AppContext } from '../../App';
 
-const CardShopping = ({id, item}) => {
+const CardShopping = ({ id, item }) => {
 
   const calculateSumOrder = () => {
     let sum = Number(0);
 
     for (let key in item) {
-      if(key === 'id') {
+      if (key === 'id') {
         break;
       }
       sum += Number(item[key].price);
@@ -16,28 +16,35 @@ const CardShopping = ({id, item}) => {
     return sum;
   }
 
-  console.log(item);
+  const handleItemToArray = () => {
+    const arr = [];
+
+    for (let key in item) {
+      if (key === 'id') {
+        break;
+      }
+      arr[key] = item[key];
+    }
+
+    return arr;
+  }
+
+  console.log(handleItemToArray());
+
   return (
     <li>
       <article className='card-shopping'>
         <h3 className='card-shopping__title'>Заказ №{id}</h3>
-        <div className='card-shopping__container'>
-          <div className='card-shopping__container-item'>
-            <img className='card-shopping__item-img' src='./images/card-item-1.jpg' alt='' />
-          </div>
-          <div className='card-shopping__container-item'>
-            <img className='card-shopping__item-img' src='./images/card-item-1.jpg' alt='' />
-          </div>
-          <div className='card-shopping__container-item'>
-            <img className='card-shopping__item-img' src='./images/card-item-1.jpg' alt='' />
-          </div>
-          <div className='card-shopping__container-item'>
-            <img className='card-shopping__item-img' src='./images/card-item-1.jpg' alt='' />
-          </div>
-          <div className='card-shopping__container-item'>
-            <img className='card-shopping__item-img' src='./images/card-item-1.jpg' alt='' />
-          </div>
-        </div>
+        <ul className='card-shopping__container'>
+          <span className='card-shopping__count'>{handleItemToArray().length} шт.</span>
+          {
+            handleItemToArray().map((item, index) =>
+              <li key={index} className='card-shopping__container-item'>
+                <img className='card-shopping__item-img' src={item.linkImg} alt='' />
+              </li>
+            )
+          }
+        </ul>
         <div className='card-shopping__info'>
           <p className='card-shopping__info-text'>{calculateSumOrder()} руб.</p>
           <p className='card-shopping__info-text'>{item[0].date}</p>
