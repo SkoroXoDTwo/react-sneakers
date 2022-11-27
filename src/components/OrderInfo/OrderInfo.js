26e44fb3ac297b28504c5f02e3ad6249a5ba15c9
Alexey Belkov <lexabelco2013@yandex.ru>
2022-11-27 15:58:58 +0500

    fix: поправить баг отображения даты заказа

--- src/components/OrderInfo/OrderInfo.js
+++ src/components/OrderInfo/OrderInfo.js
@@ -4,16 +4,20 @@ import { AppContext } from '../../App';
 
 const OrderInfo = ({ isOpened, close }) => {
   const [numberRandomEmoji, setNumberRandomEmoji] = React.useState(1);
+  const [dateOrder, setDateOrder] = React.useState('');
   const { shoppingItems, calculateSumOrder } = React.useContext(AppContext);
 
   React.useEffect(() => {
+    if(!shoppingItems) {
+      const date = shoppingItems[shoppingItems.length - 1][0].date;
+      setDateOrder(date)
+    }
+
     const random = Math.floor(Math.random() * 12) + 1;
     setNumberRandomEmoji(random);
   }, [isOpened]);
 
- const getDateOrder = () => {
-  return shoppingItems[shoppingItems.length - 1][0].date;
- }
+
 
   return (
     <div className={'order-info ' + (isOpened ? 'order-info_opened' : '')}>
@@ -23,7 +27,7 @@ const OrderInfo = ({ isOpened, close }) => {
         <img className='order-info__img' src={`./images/emoji-fun/emoji-${numberRandomEmoji}.svg`} />
         <h2 className='order-info__title'>Заказ №{shoppingItems.length} оформлен</h2>
         <p className='order-info__text'>Сумму заказа: {calculateSumOrder(shoppingItems[shoppingItems.length - 1])} руб.</p>
-        <p className='order-info__text'>Дата заказа: {getDateOrder()}</p>
+        <p className='order-info__text'>Дата заказа: {dateOrder}</p>
       </div>
     </div>
   );
